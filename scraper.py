6a8a6fad2dbbd9fab7768e6b17924cc6526423f7
inkering <inkering@gmail.com>
2017-07-03 18:46:04 -0400

    follow pep8 a little more

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
-import sys
 import requests
 import csv
 from bs4 import BeautifulSoup
+
 
 def getRugPage(rug):
     item_id = rug
@@ -9,7 +9,9 @@ def getRugPage(rug):
     rugPage = BeautifulSoup(page.content, "html.parser")
     return rugPage
 
+
 def getRugName(rugPage):
+
     try:
         rugPage = rugPage.find("div", {"class": "heading-area"})
         item_name = list(rugPage.children)[1]
@@ -20,14 +22,14 @@ def getRugName(rugPage):
 
 def getRugLink(rugPage):
     try:
-        imagesoup = rugPage.find("img", {"id":"loupe"})
+        imagesoup = rugPage.find("img", {"id": "loupe"})
         item_url = imagesoup.get('src')
         return item_url
     except AttributeError:
         pass
 
 
-#build a id index of numbers
+# build a id index of numbers
 item_id_list = []
 
 count = 410
@@ -41,14 +43,14 @@ print("\n")
 base_url = "http://www.peterpap.com/rugDetail.cfm?rugID="
 
 with open('rugs.csv', 'w') as f:
-    writer=csv.writer(f)
+    writer = csv.writer(f)
     writer.writerow(('Name', 'ID', "Image Url"))
     for rug in item_id_list:
         RugPage = getRugPage(rug)
         RugName = getRugName(RugPage)
         RugLink = getRugLink(RugPage)
         try:
-            writer.writerow((RugName,rug,RugLink))
+            writer.writerow((RugName, rug, RugLink))
             print(RugName)
             print(RugLink)
             print(rug)
